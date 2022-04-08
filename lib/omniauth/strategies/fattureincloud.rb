ba422d78a15bdffc88e72d33aa5f972c4e862f92
Alessio Santo <alessio.santocs@gmail.com>
2022-04-08 12:24:24 +0800

    Add callback method

--- lib/omniauth/strategies/fattureincloud.rb
+++ lib/omniauth/strategies/fattureincloud.rb
@@ -27,6 +27,10 @@ module OmniAuth
         }
       end
 
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
       def raw_info
         @raw_info ||= access_token.get("/user/info").parsed
       end
